ab8d3a4a74a7c060a42c8f353b75b76bc59e6c5f
agamjeetsingh <agamjeetsingh2005@gmail.com>
2025-02-16 16:38:06 +0000

    make database a package

---
+++ database/__init__.py
@@ -0,0 +1,10 @@
+from db import SolvingProcess, Solution, ResearchDatabase, SolutionType, SolvingStep
+from test_db import test_total_verification_attempts
+
+__all__ = [
+    "SolvingProcess",
+    "Solution",
+    "ResearchDatabase",
+    "SolutionType",
+    "SolvingStep"
+]
