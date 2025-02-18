0e6bd53d97d1d22aabe605edb11c7d788aff1faf
agamjeetsingh <agamjeetsingh2005@gmail.com>
2025-02-18 02:52:05 +0000

    Update solver properties in solver.py

--- solvers/solver.py
+++ solvers/solver.py
@@ -6,14 +6,14 @@ from utils.model import Model, ModelName
 @dataclass
 class Properties:
     max_reasoning_tries: Optional[int] = 4
-    max_verifier_passes: Optional[int] = 1
+    max_verifier_passes: Optional[int] = 4
     parallel_reasoning_tries: Optional[int] = 4 # Used in feedback_and_condensed
-    reasoner_model: Optional[Model] = Model(ModelName.O3_MINI_MEDIUM)
+    reasoner_model: Optional[Model] = Model(ModelName.O3_MINI_HIGH)
     verifier_model: Optional[Model] = Model(ModelName.O3_MINI_HIGH)
     discussion_condenser_model: Optional[Model] = Model(ModelName.O3_MINI_HIGH)
 
 class Solver(ABC):
-    def __init__(self, problem_statement: str, properties: Properties):
+    def __init__(self, problem_statement: str, properties: Properties = Properties()):
         self.problem_statement = problem_statement
         self.properties = properties
 
