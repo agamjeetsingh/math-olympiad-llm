aecfaaef8b7eeb6a417cf55874c322e282601548
agamjeetsingh <agamjeetsingh2005@gmail.com>
2025-02-18 02:48:58 +0000

    Add new solvers to __all__

--- solvers/__init__.py
+++ solvers/__init__.py
@@ -1,5 +1,7 @@
 from .solver import Solver, Properties
 from .no_feedback import NoFeedback
 from .feedback import Feedback
+from .feedback_and_condensed import FeedbackAndCondensed
+from .deep_check import DeepCheck
 
-__all__ = ['Solver', 'Properties', 'NoFeedback', 'Feedback']
+__all__ = ['Solver', 'Properties', 'NoFeedback', 'Feedback', 'FeedbackAndCondensed', 'DeepCheck']
